1fbb6a24214a257300d91935767f9110f8ab40b8
Nicolas Ettlin <nicolas@convex.dev>
2026-05-10 11:36:20 -0700

    Revert "Avoid using streams"
    This reverts commit fcc322b60a93c814f750c9dde31a8edf9060a8a1.

--- packages/render/src/edge/render.tsx
+++ packages/render/src/edge/render.tsx
@@ -1,13 +1,36 @@
-import reactDOMServer from "react-dom/server.edge";
-import { pretty } from "../node";
-import type { Options } from "../shared/options";
-import { toPlainText } from "../shared/utils/to-plain-text";
+import { Suspense } from 'react';
+import reactDOMServer from 'react-dom/server.edge';
+import { pretty } from '../node';
+import { createErrorBoundary } from '../shared/error-boundary';
+import type { Options } from '../shared/options';
+import { readStream } from '../shared/read-stream.browser';
+import { toPlainText } from '../shared/utils/to-plain-text';
 
 export const render = async (
   element: React.ReactElement,
   options?: Options,
 ) => {
-  const html = reactDOMServer.renderToString(element);
+  const html = await new Promise<string>((resolve, reject) => {
+    const ErrorBoundary = createErrorBoundary(reject);
+    reactDOMServer
+      .renderToReadableStream(
+        <ErrorBoundary>
+          <Suspense>{element}</Suspense>
+        </ErrorBoundary>,
+        {
+          onError(error: unknown) {
+            reject(error);
+          },
+          progressiveChunkSize: Number.POSITIVE_INFINITY,
+        },
+      )
+      .then(async (stream) => {
+        await stream.allReady;
+        return readStream(stream);
+      })
+      .then(resolve)
+      .catch(reject);
+  });
 
   if (options?.plainText) {
     return toPlainText(html, options.htmlToTextOptions);
@@ -16,7 +39,7 @@ export const render = async (
   const doctype =
     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
 
-  const document = `${doctype}${html.replace(/<!DOCTYPE.*?>/, "")}`;
+  const document = `${doctype}${html.replace(/<!DOCTYPE.*?>/, '')}`;
 
   if (options?.pretty) {
     return pretty(document);
